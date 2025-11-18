516ec73f681079601d341f3b3dc4b61157639f75
Francesco La Camera <fm@lacamera.org>
2025-11-18 20:29:16 +0100

    build(website): vendor chunks

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +9,15 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mui/material'],
+        },
       },
     },
   },
